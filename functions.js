51de0e2d5a61ebf3016d6de06632aaf20bd7dcff
PalumboN <nahuel.palumbo@gmail.com>
2020-04-19 03:56:20 -0300

    Parametric check

--- functions.js
+++ functions.js
@@ -14,22 +14,17 @@ const isFullyBlockInput = input => isBlockInput(input) && !isEmptyInput(input)
 
 const isFunction = type => type.includes("->")
 
+const isVarType = type => type == type.toLowerCase()
+
 const asFunctionType = (...types) => types.join('->')
 
 const functionTypeToList = functionType => functionType.split('->')
 
-function inferType(type, typeMap) {
-  return Object.entries(typeMap)
-    .reduce((type, [param, value]) => type.replace(param, value), type)
-}
-
 function functionType(functionBlock) {
-  const typeMap = typeVariables(functionBlock)
   const inputTypes = functionBlock.inputList
     .filter(isEmptyBlockInput)
     .map(input => getTypeInput(input))
-  const parametricType = asFunctionType(...inputTypes, getType(functionBlock.outputConnection))
-  return inferType(parametricType, typeMap)
+  return asFunctionType(...inputTypes, getType(functionBlock.outputConnection))
 }
 
 function outputFunctionType(functionBlock) {
@@ -46,21 +41,36 @@ function typeVariables(functionBlock) {
     .forEach(input => {
       const typeVar = input.parametricType
       const type = getType(input.connection.targetConnection)
-      typeMap[typeVar] = type //TODO: Type check? 
+      typeMap[typeVar] = typeMap[typeVar] && typeMap[typeVar] != type ? "ERROR" : type  //TODO: Type check? 
     })
   return typeMap
 }
 
 function getTypeInput(input) {
-  return input.parametricType || getType(input.connection)
+  if (input.parametricType) {
+    const typeMap = typeVariables(input.getSourceBlock())
+    return typeMap[input.parametricType] || input.parametricType
+  }
+
+  return getType(input.connection)
 }
 
 function getType(connection) {
   return connection.getCheck() && connection.getCheck()[0] || 'Any'
 }
 
-function checkType(connection, block, getType = functionType) {
+function checkConnectionType(connection, block, getType = functionType) {
   return connection.checkType({ check_: [getType(block)] })
+}
+
+function checkInputType(input, block) {
+  const inputType = getTypeInput(input)
+  const blockType = functionType(block)
+  return inputType != 'Error' && blockType != 'Error' && (
+    inputType == 'Any' || blockType == 'Any'
+    || isVarType(inputType) || isVarType(blockType)
+    || inputType == blockType //TODO: Match types
+  )
 }
 
 function bump(block) {
@@ -74,17 +84,17 @@ function firstEmptyFree(block) {
 
 function matchCompositionType(block1, block2) {
   const input = firstEmptyFree(block1)
-  return input && checkType(input.connection, block2, outputFunctionType)
+  return input && checkConnectionType(input.connection, block2, outputFunctionType)
 }
 
 function matchApplyType(block1, block2) {
   const input = firstEmptyFree(block1)
-  return input && checkType(input.connection, block2)
+  return input && checkConnectionType(input.connection, block2)
 }
 
-function checkParentConnection(functionBlock) {
-  if (functionBlock.outputConnection.targetConnection && !checkType(functionBlock.outputConnection.targetConnection, functionBlock)) {
-    bump(functionBlock)
+function checkParentConnection(block) {
+  if (block.outputConnection.targetConnection && !checkInputType(block.outputConnection.targetConnection.getParentInput(), block)) {
+    bump(block)
   }
 }
 
@@ -246,11 +256,11 @@ Blockly.Blocks['composition'] = {
 Blockly.Blocks["math_arithmetic"].onchange = function (event) { onChangeFunction.bind(this)(event) }
 Blockly.Blocks["math_number"].onchange = function (event) {
   if (event.blockId == this.id) {
-    if (!this.getParent()) {
-      this.setColour(230)
-    } else {
-      this.setColour(30)
-    }
-
+    checkParentConnection(this)
+  }
+}
+Blockly.Blocks["text"].onchange = function (event) {
+  if (event.blockId == this.id) {
+    checkParentConnection(this)
   }
 }
