b4877ccc5007e7ceb807253f41a14e65fab484a2
PalumboN <nahuel.palumbo@gmail.com>
2020-04-27 04:01:51 -0300

    Divide in files

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+const isBlockInput = input => input.type === 1
+
+const isEmptyInput = input => !input.connection.targetConnection
+
+const isEmptyBlockInput = input => isBlockInput(input) && isEmptyInput(input)
+
+const isFullyBlockInput = input => isBlockInput(input) && !isEmptyInput(input)
