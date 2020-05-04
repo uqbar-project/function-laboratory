f3d9b2f2045588d2e5efd7194c482af231550de5
juanFds <juan9794@gmail.com>
2020-05-04 20:53:47 -0300

    Renamed color specs test names

--- test/colorSpec.js
+++ test/colorSpec.js
@@ -5,17 +5,17 @@ describe('Colors', () => {
   const Boolean = createType('Boolean')
   const Number = createType('Number')
 
-  onWorkspace('value', workspace => {
+  onWorkspace('value color is the color of the type', workspace => {
     const number = workspace.newBlock('math_number')
     assertColor(number, typeToColor(Number))
   })
 
-  onWorkspace('function', workspace => {
+  onWorkspace('function color is the sum of the color of its inputs types and its output type', workspace => {
     const even = workspace.newBlock('even')
     assertColor(even, typeToColor(Number) + typeToColor(Boolean))
   })
 
-  onWorkspace('partial applied function with same type', workspace => {
+  onWorkspace('partial applied function color is the same as the color of a function whose type is the same as the the partial applied function s type', workspace => {
     const even = workspace.newBlock('even')
     const compare = workspace.newBlock('compare')
     const number = workspace.newBlock('math_number')
@@ -23,7 +23,7 @@ describe('Colors', () => {
     assertColor(compare, colorType(even))
   })
 
-  onWorkspace('applied function', workspace => {
+  onWorkspace('applied function color is the color of its output type', workspace => {
     const even = workspace.newBlock('even')
     const number = workspace.newBlock('math_number')
     connect(even, number)
@@ -31,7 +31,7 @@ describe('Colors', () => {
     assertColor(number, typeToColor(Boolean))
   })
 
-  onWorkspace('applied function with functions', workspace => {
+  onWorkspace('applied function with an applied function as its parameter color is the color of its output type', workspace => {
     const not = workspace.newBlock('not')
     const even = workspace.newBlock('even')
     const number = workspace.newBlock('math_number')
